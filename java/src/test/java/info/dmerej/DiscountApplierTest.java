3bcc2d983f7fd71077d7efdba408ec08389ec98d
JiveOff <antoine@jiveoff.fr>
2024-11-19 15:36:48 +0100

    chore: DiscountApplierTest for v2

--- java/src/test/java/info/dmerej/DiscountApplierTest.java
+++ java/src/test/java/info/dmerej/DiscountApplierTest.java
@@ -29,19 +29,19 @@ public class DiscountApplierTest {
   @Test
   void should_notify_twice_when_applying_discount_for_two_users_v2() {
     //GIVEN
-    User user = new User("user","user@gmail.com");
-    User user1 = new User("user","user1@gmail.com");
+    User user1 = new User("user1","user1@gmail.com");
+    User user2 = new User("user1","user2@gmail.com");
 
-    List<User> users = Arrays.asList(user1,user);
+    List<User> users = Arrays.asList(user1,user2);
     NotifierStub notifier = new NotifierStub();
     DiscountApplier discountApplier = new DiscountApplier(notifier);
 
     //WHEN
-    discountApplier.applyV1(50, users);
+    discountApplier.applyV2(50, users);
 
     //THEN
-    Assertions.assertEquals(1, notifier.getMessagesCount(user));
     Assertions.assertEquals(1, notifier.getMessagesCount(user1));
+    Assertions.assertEquals(1, notifier.getMessagesCount(user2));
   }
 
 }
